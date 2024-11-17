15c93111803dfd12be8e9ce303a4e36737a490b7
developerPower <150381590+developerPower@users.noreply.github.com>
2024-11-17 19:29:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -185,7 +185,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:8080/auth/google/callback",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || "https://crm-gc6q.onrender.com/auth/google/callback",
     proxy: true
 }, async (accessToken, refreshToken, profile, done) => {
     try {
